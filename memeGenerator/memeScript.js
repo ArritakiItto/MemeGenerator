cd562d3280a8057304303680939ee42ed22bb082
arritakiItto <kalevisual89@gmail.com>
2022-11-22 20:13:27 -0800

    Test changes to old MemeGenerator codes to newer

--- memeGenerator/memeScript.js
+++ memeGenerator/memeScript.js
@@ -1,3 +1,36 @@
+// let img = document.querySelector("#image-file");
+// let form = document.querySelector("form")
+// let topTxt = document.querySelector("#top-text");
+// let bottomTxt = document.querySelector("#bottom-text");
+// const button = document.querySelector("#submit");
+
+// form.addEventListener("submit", function(event){
+//     event.preventDefault();
+//     const memeImage = document.createElement("div");
+//     const textTop= document.createElement("div");
+//     const textBottom = document.createElement("div");
+//     const imgFile = document.getElementById("image-file");
+
+//     imgFile.src = document.getElementById("image-file").value;
+//     textTop.classList.add("top-text");
+//     textTop.innerHTML = document.getElementById("top-text").value;
+
+//     textBottom.classList.add("bottom-text");
+//     textBottom.innerHTML = document.getElementById("bottom-text").value;
+      
+
+//     memeImage.classList.add("memeImage");
+//     memeImage.append(textTop);
+//     memeImage.append(textBottom);
+//     memeImage.append(imgFile);
+//     let memeCanvas = document.getElementById("canvas");
+//     memeCanvas.append(memeImage);
+
+// form.reset()
+   
+//     });
+
+
 let img = document.querySelector("#image-file");
 let form = document.querySelector("form")
 let topTxt = document.querySelector("#top-text");
